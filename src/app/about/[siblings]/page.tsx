3777d430b860d6ce9218d192d3129ec698179bee
DAVINA SINGH <davina_singh@outlook.com>
2023-11-21 15:51:51 +0000

    dynamic route parameter

--- src/app/about/[siblings]/page.tsx
+++ src/app/about/[siblings]/page.tsx
@@ -2,12 +2,21 @@ import Link from 'next/link'
 
 type siblingParam = {params: {siblings: string}}
 
-export default function Page({params}: siblingParam ) {
+export function generateMetadata({params}: siblingParam ) {
     const modTitle = params.siblings[0].toUpperCase() + params.siblings.slice(1)
     return (
         {
             title: `Patch's Siblings - ${modTitle}`,
             description: `Patch's Siblings - fun facts about ${params.siblings}`
         }
+    )
+}
+
+export default function Page({params}: siblingParam ) {
+    return (
+        <div>
+            <h2>This is my dynamic route paramater: {params.siblings} </h2>
+            <Link href='/about'>{`<-- Go back`}</Link>
+        </div>
     )
 }
